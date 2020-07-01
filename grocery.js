6644cb43a4d57af1b8e102ff6f2c5ee472d37a43
vfsettle <56850679+vfsettle@users.noreply.github.com>
2020-06-30 19:05:30 -0700

    Stuck on Printing Array Elements

--- grocery.js
+++ grocery.js
@@ -1,14 +1,22 @@
 var Grocery = /** @class */ (function () {
-    function Grocery(item, price, quantity) {
-        this.item = item;
+    function Grocery(itemName, price, quantity) {
+        this.itemName = itemName;
         this.price = price;
         this.quantity = quantity;
-        this.groceryItem = item + " " + price + " " + quantity;
+        this.itemInfo = itemName + " " + price + " " + quantity;
     }
     return Grocery;
 }());
-function review(cart) {
-    return "Item: " + cart.item + " " + "Price: $" + cart.price + " " + "Quantity: " + cart.quantity;
+var cart = [];
+cart.push(new Grocery("Eggs", "2.99", "1"));
+cart.push(new Grocery("Milk", "4.99", "1"));
+cart.push(new Grocery("Bacon", "7.99", "1"));
+function displayCart(cart) {
+    for (var _i = 0, cart_1 = cart; _i < cart_1.length; _i++) {
+        var i = cart_1[_i];
+        console.log(i);
+    }
+    return "Hello, here is what is in your cart: " + cart;
 }
-var basket = new Grocery("Eggs", 3.99, 1);
-document.body.textContent = review(basket);
+//let shopper = ["item1", "item2", "item3"];
+document.body.textContent = displayCart(cart);

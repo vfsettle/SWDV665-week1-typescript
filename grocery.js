1a95f0d26f10c401971f0e612f2c9df3b75ad340
vfsettle <56850679+vfsettle@users.noreply.github.com>
2020-07-06 18:42:26 -0700

    latest commit incorporating item interface

--- grocery.js
+++ grocery.js
@@ -1,20 +1,32 @@
-var Grocery = /** @class */ (function () {
-    function Grocery(name, price, qty) {
+var Item = /** @class */ (function () {
+    function Item(name, price, dept) {
         this.name = name;
         this.price = price;
-        this.qty = qty;
+        this.dept = dept;
+        this.item = name + " " + price + " " + dept;
     }
-    return Grocery;
+    return Item;
 }());
-var cart = [];
-cart.push(new Grocery("Eggs", "2.99", "1"));
-cart.push(new Grocery("Milk", "4.99", "1"));
-cart.push(new Grocery("Bacon", "7.99", "1"));
-function displayCart(cart) {
+function getItemValues(item) {
+    return item.name + " " + item.price + " " + item.dept;
+}
+var cart = [new Item("Eggs", "2.99", "1"), new Item("Milk", "4.99", "1"), new Item("Bacon", "7.99", "1")];
+//cart.push( new Item("Eggs","2.99","1"));
+//cart.push( new Item("Milk","4.99","1"));
+//cart.push( new Item("Bacon","7.99","1")); 
+function printCart(cart) {
     for (var _i = 0, cart_1 = cart; _i < cart_1.length; _i++) {
         var i = cart_1[_i];
-        console.log(i);
+        getItemValues(i);
     }
-    return "Hello, here is what is in your cart: " + cart;
+    return cart;
 }
-document.body.textContent = displayCart(cart);
+document.body.textContent = printCart(cart);
+//I think it would help if you didn't use the console.log and wrote the script directly. 
+//The way I would do it is after I declare my function name and parameters, 
+//I would declare a variable that is my print string template 
+//(this is what I want where), then have the loop cycle through it. 
+//It would then return this string variable that has been looped. 
+//Right now you're printing to the console which is fine 
+//but is really a testing method to do. 
+//You might want to just be more specific in what you want placed.
--- grocery.ts
+++ grocery.ts
@@ -1,20 +1,42 @@
-class Grocery {
+class Item {
     item: string;
-    constructor(public name: string, public price: string, public qty: string) {
-    }
+    constructor(public name: string, public price: string, public dept: string) {
+        this.item = name + " " + price + " " + dept;
+    }    
     
 }
 
-let cart = [];
-cart.push(new Grocery("Eggs","2.99","1"));
-cart.push(new Grocery("Milk","4.99","1"));
-cart.push(new Grocery("Bacon","7.99","1"));
-
-function displayCart(cart: string[]) {
-    for (let i of cart) {
-        console.log(i)
-    }
-    return "Hello, here is what is in your cart: " + cart;
+interface Item {
+    name: string;
+    price: string;
+    dept: string;
 }
 
-document.body.textContent = displayCart(cart);
+function getItemValues (item:Item) {
+    return item.name + " " + item.price + " " + item.dept; 
+}
+
+
+let cart = [new Item("Eggs","2.99","1"),new Item("Milk","4.99","1"), new Item("Bacon","7.99","1") ];
+//cart.push( new Item("Eggs","2.99","1"));
+//cart.push( new Item("Milk","4.99","1"));
+//cart.push( new Item("Bacon","7.99","1")); 
+
+function printCart(cart) {
+    for (let i of cart) {
+        getItemValues(i)
+    }
+    return cart;
+}
+
+document.body.textContent = printCart(cart);
+
+
+//I think it would help if you didn't use the console.log and wrote the script directly. 
+//The way I would do it is after I declare my function name and parameters, 
+//I would declare a variable that is my print string template 
+//(this is what I want where), then have the loop cycle through it. 
+//It would then return this string variable that has been looped. 
+//Right now you're printing to the console which is fine 
+//but is really a testing method to do. 
+//You might want to just be more specific in what you want placed.
